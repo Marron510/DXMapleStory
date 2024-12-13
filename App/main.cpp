304f385606eb70087feb00e8d4d8d37cc8e67317
Marron0510 <wkdtl345@naver.com>
2024-12-13 12:00:03 +0900

    241213_엔진 업데이트

--- App/main.cpp
+++ App/main.cpp
@@ -4,7 +4,13 @@
 #pragma comment(lib, "EnginePlatform.lib")
 #pragma comment(lib, "EngineCore.lib")
 
-int main()
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+
 {
+
+
 	return 0;
 }
