765520a5d905cd9613e33121d27fd5fb05886e35
Marron0510 <wkdtl345@naver.com>
2025-01-02 18:17:38 +0900

    250102_업데이트

--- EngineCore/EngineCamera.h
+++ EngineCore/EngineCamera.h
@@ -3,11 +3,8 @@
 #include <list>
 #include "Renderer.h"
 #include "SceneComponent.h"
+#include "EngineEnums.h"
 
-// MinimapCamera;
-// PlayerCamera
-// PlayCamera
-// UICamera
 
 // Ό³Έν :
 class UEngineCamera : public USceneComponent
@@ -33,16 +30,25 @@ public:
 
 	void CalculateViewAndProjection();
 
-	void SetFar(float _Value)
+	ENGINEAPI void SetFar(float _Value)
 	{
 		Far = _Value;
 	}
 
-	void SetNear(float _Value)
+	ENGINEAPI void SetNear(float _Value)
 	{
 		Near = _Value;
 	}
 
+	ENGINEAPI void SetFOV(float _Value)
+	{
+		FOV = _Value;
+	}
+
+	ENGINEAPI void SetProjectionType(EProjectionType _Type)
+	{
+		Type = _Type;
+	}
 
 protected:
 
@@ -50,6 +56,10 @@ protected:
 private:
 	float Near = 1.0f;
 	float Far = 5000.0f;
+
+	float FOV = 60.0f;
+
+	EProjectionType Type = EProjectionType::Orthographic;
 
 	FVector ProjectionScale = { 0.0f, 0.0f };
 
