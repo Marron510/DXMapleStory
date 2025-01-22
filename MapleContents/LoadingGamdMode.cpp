c621f7ae19879427b8f025d5276e28cf5bab6f2a
Marron0510 <wkdtl345@naver.com>
2025-01-22 14:47:57 +0900

    250122_소드오러 업데이트

--- MapleContents/LoadingGamdMode.cpp
+++ MapleContents/LoadingGamdMode.cpp
@@ -720,6 +720,20 @@ void ALoadingGamdMode::Tick(float _DeltaTime)
 				UEngineSprite::CreateSpriteToFolder(Dir.GetPathToString());
 			}
 
+			{
+				UEngineDirectory Dir;
+				if (false == Dir.MoveParentToDirectory("MapleResources"))
+				{
+					MSGASSERT("리소스 폴더를 찾지 못했습니다.");
+					return;
+				}
+				Dir.Append("Image");
+				Dir.Append("Phase1");
+				Dir.Append("Phase1_Seren/Phase1_SwordAura");
+
+				UEngineSprite::CreateSpriteToFolder(Dir.GetPathToString());
+			}
+
 			// 이미지를 변환 
 			UEngineCore::CreateLevel<ATitleGameMode, APawn, ATitleHUD>("Title");
 			UEngineCore::CreateLevel<ACerniumPlazaMode, APlayer, ACerniumHUD>("Plaza");
